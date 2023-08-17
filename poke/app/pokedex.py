b592e80440f779ae0f09eeca1c5c0948f51a3d68
Marcos Alexander <marcosavila3005@gmail.com>
2023-08-17 00:31:50 -0500

    Aqui se ya la Seccion Posts Generales esta hecho, los usuarios pueden postear cosas y todos los que tengan cuenta podran ver el post

--- poke/app/pokedex.py
+++ poke/app/pokedex.py
@@ -90,10 +90,43 @@ def pokemones():
     return render_template('menus/friends.html', imagen_base64=imagen_base64 if session else None, friends = users_list, imagen_users_base64 = imagen_users_base64 )
 
 
-@bppoke.route('/posts')
+@bppoke.route('/posts', methods=['POST', 'GET'])
 @login_required
 def post():
     db, c = get_db()
+    error = None
+
+    id_user = g.user['id_user']
+
+    # Seleccionar mensajes y datos de usuario (imagen y nombre)
+   # Seleccionar mensajes y datos de usuario (imagen y nombre)
+    c.execute('SELECT up.message, up.fecha, up.users_id_post, u.name, u.imagen FROM user_post up JOIN users u ON up.users_id_post = u.id_user')
+    user_data = c.fetchall()
+    all_message = []
+
+    for post in user_data:
+        message = post['message']
+        fecha = post['fecha']
+        id_post = post['users_id_post']
+        name = post['name']
+        imagen_base64 = post['imagen']
+
+        # Decodificar la imagen desde base64
+        imagen_decoded = base64.b64encode(imagen_base64).decode('utf-8') if imagen_base64 else None
+        
+        # Crear un diccionario con la información del mensaje
+        message_info = {
+            'message': message,
+            'fecha': fecha,
+            'id': id_post,
+            'name': name,
+            'imagen': imagen_decoded
+        }
+        
+        all_message.append(message_info)
+    #Aqui se muestran los posts de todos Ends
+
+    #aqui
 
     # Obtener el nombre del usuario de la base de datos
 
@@ -104,13 +137,26 @@ def post():
         user_data = c.fetchone()
         imagen_path = user_data['imagen'] if user_data else None
         user_name = user_data['name']
+        imagen_base64 = base64.b64encode(imagen_path).decode('utf-8') if imagen_path else None
+
     else:
         return abort(401)
 
     # Antes del bloque if request.method == 'POST':
-    imagen_base64 = base64.b64encode(imagen_path).decode('utf-8') if imagen_path else None
+    if request.method == 'POST':
+        message_user = request.form['message_user']
+        id_user = g.user['id_user']
+        if not message_user:
+            error = 'Se necesita algun comentario para poder postear'
+            return render_template('contenido/posts.html', error = error)
 
-    return render_template('contenido/posts.html', imagen_base64=imagen_base64, username = user_name)
+        c.execute('INSERT INTO user_post (message, users_id_post) VALUES (%s, %s)', (message_user, id_user,))
+        db.commit()
+        return redirect(url_for(request.endpoint))
+
+    
+
+    return render_template('contenido/posts.html', imagen_base64=imagen_base64, username = user_name, all_message = all_message, all_img = imagen_decoded)
 
 
 # pokemon_name = "pikachu"
